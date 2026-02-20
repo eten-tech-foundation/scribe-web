1e10dc45441acaa51fff1c14bd08a9c58174b26c
kaseywright <kasey.wright@biblionexus.org>
2026-02-20 10:30:02 -0500

    Update magic number to use UserRole enum

--- src/components/header/MainMenu.tsx
+++ src/components/header/MainMenu.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next';
 
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { useAuth } from '@/hooks/useAuth';
+import { UserRole } from '@/lib/types';
 import { useAppStore } from '@/store/store';
 
 import MenuItem from './MenuItem';
@@ -53,7 +54,7 @@ const MainMenu: React.FC<MainMenuProps> = ({
             onClick={onDashboardClick}
             onClosePopover={() => setOpen(false)}
           />
-          {userdetail?.role === 1 && (
+          {userdetail?.role === UserRole.PROJECT_MANAGER && (
             <>
               <MenuItem
                 icon={<Kanban size={18} />}
